74ef9ada8203af20d0f04b4ab9c0cf1d040cd6ec
Ola Buczma <01178516@pw.edu.pl>
2022-11-20 17:23:42 +0100

    tests v2

--- test_paragony.py
+++ test_paragony.py
@@ -2,6 +2,10 @@ import paragony
 
 
 def test_split_price():
-    assert paragony.split_price(100) == (1, 00)
+    price_zl, price_gr = paragony.split_price(100)
+    assert price_zl == 1
+    assert price_gr == 0
 
 
+def test_pass():
+    assert 2 == 2
