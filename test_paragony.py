31e73a92ea14b3f52ea56ca1c082d9a4c3c5fed7
Ola Buczma <01178516@pw.edu.pl>
2022-11-21 12:11:35 +0100

    added tests split_price

--- test_paragony.py
+++ test_paragony.py
@@ -1,13 +1,19 @@
 import paragony
 
 
-def test_split_price():
+def test_split_price_typical():
     price_zl, price_gr = paragony.split_price(123)
+    assert price_zl == 1
+    assert price_gr == 23
+
+
+def test_split_price_gr_less_than_10():
+    price_zl, price_gr = paragony.split_price(101)
     assert price_zl == 1
     assert price_gr == 1
 
 
-def test_split_price_less_than_10():
+def test_split_price_zl_less_than_10():
     price_zl, price_gr = paragony.split_price(15)
     assert price_zl == 0
     assert price_gr == 15
